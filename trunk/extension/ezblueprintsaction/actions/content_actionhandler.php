5dc98db3b4cbb31e83aef1729e9d438ca6fdd614
tcv_br <info@example.com>
2011-09-15 20:30:49 +0000

    added action extension sample, needs documentation.
    git-svn-id: http://svn.projects.ez.no/ezblueprints@4 e420f575-cf56-41ac-8702-95b3b2c1bc47

---
+++ trunk/extension/ezblueprintsaction/actions/content_actionhandler.php
@@ -0,0 +1,13 @@
+<?php
+ 
+function ezblueprintsaction_ContentActionHandler(&$module, &$http,
+        &$objectID ) {
+ 
+    if ($http->hasPostVariable("hello" ) ) {
+        echo $http->postVariable("hello");
+        eZExecution::cleanExit();
+ 
+    }
+    return;
+}
+?>
